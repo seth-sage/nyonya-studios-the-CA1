97dd10df02d67a9c6d39dc52781ee0b11ff61afc
julia-peh <ailujhepp@gmail.com>
2026-04-29 10:51:18 +0800

    Julia- Add game obj + spite change +script change

--- Assets/Scripts/Julia.cs
+++ Assets/Scripts/Julia.cs
@@ -6,7 +6,7 @@ public class Julia : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Julia :D");
+        Debug.Log("Julia edited");
     }
 
     // Update is called once per frame
